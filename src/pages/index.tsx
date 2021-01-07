f3332b3e737c4dca216236b32e3a7eb8650d62dc
tarrows <c10t@fa.ke>
2021-01-07 23:31:24 +0900

    introduce chakra-ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
+import { Heading } from "@chakra-ui/react"
 
-const IndexPage = () => <>it works</>
+const IndexPage = () => <Heading>it works</Heading>
 
 export default IndexPage
