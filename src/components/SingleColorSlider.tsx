7c4f48f95bc9cce854daf6be4022bbc331d70bb2
tarrows <c10t@fa.ke>
2021-01-10 23:55:20 +0900

    Create SingleColorSlider first version

--- src/components/SingleColorSlider.tsx
+++ src/components/SingleColorSlider.tsx
@@ -1,1 +1,49 @@
 import React from 'react'
+
+import {
+  Box,
+  Flex,
+  NumberInput,
+  NumberInputField,
+  NumberInputStepper,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react"
+
+export type Props = {
+  initialValue?: number
+  // onChangeEnd: (val: number) => void
+}
+
+export const SingleColorSlider: React.FC<Props> = (props) => {
+  const defaultValue = props.initialValue ?? 128
+  const minValue = 0
+  const maxValue = 255
+  const [current, setCurrent] = React.useState(defaultValue)
+
+  const handleChange = (value: number) => {
+    setCurrent(value)
+  }
+
+  return (
+    <Flex>
+      <Slider flex="1" value={current} min={minValue} max={maxValue} onChange={handleChange}>
+        <SliderTrack>
+          <SliderFilledTrack />
+        </SliderTrack>
+        <SliderThumb />
+      </Slider>
+      <NumberInput maxW="100px" mr="2rem" value={current} min={minValue} max={maxValue} onChange={(_, num) => handleChange(num)}>
+        <NumberInputField />
+        <NumberInputStepper>
+          <NumberIncrementStepper />
+          <NumberDecrementStepper />
+        </NumberInputStepper>
+      </NumberInput>
+    </Flex>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Heading } from "@chakra-ui/react"
+import { SingleColorSlider } from '../components/SingleColorSlider'
 
-const IndexPage = () => <Heading>it works</Heading>
+const IndexPage = () => {
+  return (
+    <SingleColorSlider initialValue={30} />
+  )
+}
 
 export default IndexPage
